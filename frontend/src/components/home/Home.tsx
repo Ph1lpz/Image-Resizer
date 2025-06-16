2635130e5d71dec88188dbf1b27452a67d1ff2a9
Ph1lpz <znoa60@gmail.com>
2025-06-16 20:40:34 +0300

    error on wrong file type

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -5,6 +5,10 @@ import ResizeArea from "../resizeArea/ResizeArea";
 function Home() {
   const [file, setFile] = useState(null);
   const [previewUrl, setPreviewUrl] = useState(null);
+  const [fileTypeError, setFileTypeError] = useState({
+    message: "",
+    fileErr: false,
+  });
   const fileTypes = ["JPG", "PNG", "GIF"];
 
   const handleChange = (file) => {
@@ -22,12 +26,31 @@ function Home() {
     };
   }, [previewUrl]);
 
+  useEffect(() => {
+  if (fileTypeError.fileErr) {
+    const timer = setTimeout(() => {
+      setFileTypeError(prev => ({
+        ...prev,
+        message: '',
+        fileErr: false
+      }));
+    }, 8000);
+
+    return () => clearTimeout(timer); 
+  }
+}, [fileTypeError.fileErr])
+
   return (
     <>
       {file ? (
         <ResizeArea image={previewUrl} imageFile={file} />
       ) : (
         <main className="h-[95vh] w-[80%] mx-auto relative">
+          {fileTypeError.fileErr && (
+            <div role="alert" className="alert alert-error alert-soft mt-4">
+              <span>Error! {fileTypeError.message}</span>
+            </div>
+          )}
           <div className="mt-30">
             <h1 className="text-4xl font-semibold">Pixel Flow</h1>
             <p className="text-xl">a simple image resizer</p>
@@ -42,6 +65,13 @@ function Home() {
             handleChange={handleChange}
             name="file"
             types={fileTypes}
+            onTypeError={(err) => {
+              setFileTypeError({
+                ...fileTypeError,
+                message: err,
+                fileErr: true,
+              });
+            }}
           >
             <div className="w-[900px] h-[300px] bg-primary mx-auto mt-20 rounded-lg border border-gray-200 shadow-lg max-lg:w-auto">
               <FaImage className="text-7xl mx-auto mt-16 inputImage" />
@@ -60,6 +90,5 @@ function Home() {
     </>
   );
 }
-
 
 export default Home;
